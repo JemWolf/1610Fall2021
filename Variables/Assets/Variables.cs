740bf6f09c300f5d2d301c4d23038ed9803853e3
Jaiden Moffitt <jaiden@moffitts.us>
2021-09-30 23:03:45 -0600

    added variables

---
+++ Variables/Assets/Variables.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+ int x=6, y=10, z=0
+
+ z=x*y
+
+Console.WriteLine(z);
+}
