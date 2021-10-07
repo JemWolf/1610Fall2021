3aace98dcb144e9831764624ad1289be526f3703
Jaiden Moffitt <jaiden@moffitts.us>
2021-10-06 21:22:54 -0600

    created if/then statement

--- Conditionals_and_Switches/Assets/Conditions.cs
+++ Conditionals_and_Switches/Assets/Conditions.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Conditions : MonoBehaviour
 {
+    void Start ()
+    {
+        if (16  > 13)
+        {
+            Debug.Log("16 is greater than 13");
+        }
+    }
+  
 
 }
