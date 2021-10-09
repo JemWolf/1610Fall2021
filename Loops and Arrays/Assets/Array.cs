b36fdb2018c45cf76d9b777b19f0952135c48664
Jaiden Moffitt <jaiden@moffitts.us>
2021-10-08 18:54:43 -0600

    Created Array

---
+++ Loops and Arrays/Assets/Array.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] dogs = { "Chihuahua", "Husky", "Yorkie", "German Shepherd" };
+    }
+}
