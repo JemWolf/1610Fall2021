193cb75726eb402db2d2adf0c9a5a95b6683f4d1
Jaiden Moffitt <jaiden@moffitts.us>
2021-11-10 10:49:55 -0700

    fixed typo in spawnManager code

--- Create With Code/Prototype 4/Assets/SpawnManager.cs
+++ Create With Code/Prototype 4/Assets/SpawnManager.cs
@@ -12,7 +12,7 @@ public class SpawnManager : MonoBehaviour
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos -new Vector3(spawnPosX, 0, spawnPosZ);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
     }
 
