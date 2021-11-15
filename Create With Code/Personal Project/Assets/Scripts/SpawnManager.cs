32118903b808f46be81653d1401a8ff4034cc69a
Jaiden Moffitt <jaiden@moffitts.us>
2021-11-14 22:02:21 -0700

    Spawn manager working
    new spawned obstacles fall off of plane though

--- Create With Code/Personal Project/Assets/Scripts/SpawnManager.cs
+++ Create With Code/Personal Project/Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] obstaclePrefabs;
+    private float spawnRangeX = 6;
+    private float spawnPosZ = 60;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomObstacle", 2, 1.5f);
     }
 
     // Update is called once per frame
@@ -15,4 +19,12 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+
+    void SpawnRandomObstacle()
+    {
+        int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
+    }
+
 }
