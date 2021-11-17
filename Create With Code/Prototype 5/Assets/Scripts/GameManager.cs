30acfc7c0955fafc84529b3d2dbdf9c565e35dde
Jaiden Moffitt <jaiden@moffitts.us>
2021-11-16 23:57:58 -0700

    Added difficulty buttons

--- Create With Code/Prototype 5/Assets/Scripts/GameManager.cs
+++ Create With Code/Prototype 5/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +11,7 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
+    public Button restartButton;
     private int score;
     private float spawnRate = 1.0f;
 
@@ -49,6 +52,12 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
         isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
