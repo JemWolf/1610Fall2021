023e8a5a8619d66f5db4c0126a420dd07a6ff818
Jaiden Moffitt <jaiden@moffitts.us>
2021-11-16 23:59:51 -0700

    fixed typos

--- Create With Code/Prototype 5/Assets/Scripts/DifficultyButton.cs
+++ Create With Code/Prototype 5/Assets/Scripts/DifficultyButton.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
-    private DifficultyButton button;
+    private Button button;
 
     // Start is called before the first frame update
     void Start()
     {
-        button = GetComponent<button>();
+        button = GetComponent<Button>();
         button.onClick.AddListener(SetDifficulty);
     }
 
